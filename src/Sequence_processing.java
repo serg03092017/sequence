52053cbdda4a25327f1544f89919997188617643
sergey <31596499+serg03092017@users.noreply.github.com>
2017-10-04 19:11:16 +0300

    2. Бесконечная последовательность Возьмём бесконечную цифровую последовательность, образованную склеиванием последовательных положительных чисел: S = 123456789101112131415... Определите первое вхождение заданной подпоследовательности A в бесконечной последовательности S (нумерация начинается с 1).
    Программа должна читать данные из stdin и выводить ответы в stdout.
    
    Пример входных данных (по одной подпоследовательности на строку, максимальная длина подпоследовательности — 50 символов):
    6789
    111
    
    Пример выходных данных:
    6
    12

--- src/Sequence_processing.java
+++ src/Sequence_processing.java
@@ -6,15 +6,7 @@ public class Sequence_processing {
         Sequence_IO num1 = new Sequence_IO(new BufferedReader(new InputStreamReader(System.in)));
 
             byte[] v = num1.readNumber(System.in, 50);
-            /*for (int i = 0; i < v.length; i++) {
-                System.out.print(v[i]);
-            }
-            */
             if (v.length == 0) return;
-
-            //System.out.println();
-            //System.out.println("length=" + v.length);
-
             Sequence_first push = new Sequence_first(v);
     }
 }
